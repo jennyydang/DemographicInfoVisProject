223542574a00de2bc71905bc006fb6861b1462e1
jennyydang <jennyydang@gmail.com>
2017-03-07 19:05:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var map;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), 
+        {
+        	center: {lat: 40.730610, lng: -73.935242},
+    		zoom: 10
+        });
+    }
